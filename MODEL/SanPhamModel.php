f6e3b451fec4f63448eb2e3fd0c61165b32feefa
Duong Thanh Truong <dttruonga8tqtpy@gmail.com>
2024-03-29 19:01:24 +0700

    update homepage

--- MODEL/SanPhamModel.php
+++ MODEL/SanPhamModel.php
@@ -50,6 +50,30 @@ class SanPhamModel {
         $conn->close();
         return $sanphamList;
     }
+    public function getListSPKhuyenMai(){
+        $this->getInstance();
+        $db = new Database();
+        $conn = $db->getConnection();
+        $sql = "SELECT s.tenSanPham, s.giaBan, s.src, k.giaTri, k.background,SUM(cthd.soluong) AS TongSoLuongBanDuoc, k.tenKhuyenMai, dg.star
+        FROM sanpham s
+        LEFT JOIN chitietkhuyenmai ctk ON s.id = ctk.idsanpham
+        LEFT JOIN khuyenmai k ON ctk.idkhuyenmai = k.id
+        LEFT JOIN danhgia dg ON s.id = dg.idsanpham
+        LEFT JOIN chitiethoadon cthd ON s.id = cthd.idsanpham
+        WHERE k.hansudung > NOW() OR k.hansudung IS NULL AND s.id = ctk.idsanpham
+        GROUP BY cthd.idsanpham, s.tenSanPham, s.giaBan, s.src, k.giaTri, k.tenKhuyenMai, dg.star
+        ORDER BY TongSoLuongBanDuoc DESC
+        ";
+        $result = $conn->query($sql);
+        $sanphamList = array();
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $sanphamList[] = $row;
+            }
+        }
+        $conn->close();
+        return $sanphamList;
+    }
 }
 
 ?>
--- VIEWS/sign_up/sign_up.php
+++ VIEWS/sign_up/sign_up.php
@@ -77,8 +77,8 @@
     crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="/web2/STATIC/css/sign_up.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src="/web2/STATIC/js/validation.js"></script>
+    
+    
     <title>Đăng ký</title>
 </head>
 <body>
@@ -199,5 +199,7 @@
         </div>
     </div>
     </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="/web2/STATIC/js/validation.js"></script>
 </body>
 </html>
