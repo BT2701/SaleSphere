1238a37caedc9fc9d8e486544514ee617f40c514
truongduc02 <vuvuong0602@gmail.com>
2024-05-09 13:25:31 +0700

    update

--- VIEWS/admin/product/AddProduct.php
+++ VIEWS/admin/product/AddProduct.php
@@ -18,18 +18,21 @@ $dvtlist = $dvt->getList();
                         </button>
                     </div>
                     <div class="modal-body">
-                    <form action="/web2/CONTROLLER/SanPhamController.php" method="post" enctype="multipart/form-data">
+                    <form action="/web2/CONTROLLER/SanPhamController.php" method="post" enctype="multipart/form-data" id="add-form">
                             <div class="form-group">
                                 <label for="productName">Tên sản phẩm</label>
                                 <input type="text" class="form-control" id="productName" name="productName" placeholder="Nhập tên sản phẩm">
+                                <p id="productNameError" style="color: red;"></p>
                             </div>
                             <div class="form-group">
                                 <label for="productRoot">Giá nhập</label>
                                 <input type="text" class="form-control" id="productRoot" name="productRoot" placeholder="Nhập giá nhập">
+                                <p id="productRootError" style="color: red;"></p>
                             </div>
                             <div class="form-group">
                                 <label for="productPrice">Giá bán</label>
                                 <input type="text" class="form-control" id="productPrice" name="productPrice" placeholder="Nhập giá bán">
+                                <p id="productPriceError" style="color: red;"></p>
                             </div>
                             <div class="form-group">
                                 <label for="productType">Loại</label>
@@ -54,10 +57,12 @@ $dvtlist = $dvt->getList();
                             <div class="form-group">
                                 <label for="productDescription">Mô tả</label>
                                 <input type="text" class="form-control" id="productDescription" name="productDescription" placeholder="Nhập mô tả sản phẩm">
+                                
                             </div>
                             <div class="form-group">
                                 <label for="productImage">Chọn ảnh</label>
                                 <input type="file" class="form-control-file" id="productImage" name="productImage">
+                                <p id="productImageError" style="color: red;"></p>
                             </div>
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Đóng</button>
@@ -73,3 +78,45 @@ $dvtlist = $dvt->getList();
                 </div>
             </div>
         </div>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        <script>
+            $(document).ready(function () {
+  $("#saveProductBtn").click(function (event) {
+    event.preventDefault();
+
+    var formData = {
+      name: $("#productName").val(),
+      root: $("#productRoot").val(),
+      price: $("#productPrice").val(),
+      image: $("#productImage").val(),
+    };
+    $.ajax({
+      type: "POST",
+      url: "/web2/VIEWS/admin/product/validate_add.php",
+      data: formData,
+      dataType: "json",
+      encode: true,
+    }).done(function (response) {
+      if (!response.success) {
+        console.log("1");
+        if (response.errors.name1) {
+          $("#productNameError").text(response.errors.name1);
+          
+        }
+        if (response.errors.root1) {
+          $("#productRootError").text(response.errors.root1);
+        }
+        if (response.errors.price1) {
+          $("#productPriceError").text(response.errors.price1);
+        }
+        if (response.errors.image1) {
+          $("#productImageError").text(response.errors.image1);
+        }
+      } else {
+        $("#add-form").submit();
+      }
+    });
+  });
+});
+
+        </script>
